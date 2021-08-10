525d49988f1d8d3bec2031b5b0a07e6cef8e512b
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2021-08-10 00:28:36 -0300

    Update split_test.py

--- split_test.py
+++ split_test.py
@@ -4,6 +4,12 @@ import random
 data_dir = '/home/jupyter/Data/train/train'
 data_dir_valid = '/home/jupyter/Data/train/valid'
 
+try:
+  os.mkdir(data_dir)
+  os.mkdir(data_dir_valid)
+except:
+  pass
+
 files = os.listdir('/home/jupyter/Data/train/train')
 files_mv = random.sample(files,k=len(files)//10)
 
